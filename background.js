a575a4da344e89c4851da6c68fa0877d07dfc859
sintan <david1992309@gmail.com>
2015-05-23 21:26:03 +0800

    add private-IP-mode

--- background.js
+++ background.js
@@ -9,6 +9,24 @@ chrome.alarms.onAlarm.addListener(function(alarm) {
 	if (alarm.name !== "updateDDNS") {
 		return false;
 	}
+	var usePrivateIP = localStorage.getItem("option_usePrivateIP");
+	if (usePrivateIP === "true") {
+		getLocalIPs(function(privateIPs) {
+			var ip = privateIPs[0];
+			var token = localStorage.getItem("token");
+			var hostname = localStorage.getItem("hostname");
+			if (ip && token && hostname) {
+				updateCsieIoDDNS(ip, token, hostname);
+			} else {
+				console.error("onAlarm: ip or token or hostname setting error");
+				chrome.alarms.clear("updateDDNS", function(wasCleared) {
+					console.error("updateDDNS alarm clear");
+				});
+			}
+		});
+		return ;
+	}
+	// not privateIP
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', "http://api.hostip.info/get_json.php");
 	xhr.onreadystatechange = function() {
@@ -71,3 +89,40 @@ chrome.runtime.onInstalled.addListener(function(details) {
 	});
 
 });
+
+/*
+ * about function getLocalIPs
+ *
+ * source code form : http://stackoverflow.com/questions/18572365/get-local-ip-of-a-device-in-chrome-extension
+ * author : Rob W
+ * 
+ */
+function getLocalIPs(callback) {
+    var ips = [];
+
+    var RTCPeerConnection = window.RTCPeerConnection ||
+        window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+
+    var pc = new RTCPeerConnection({
+        // Don't specify any stun/turn servers, otherwise you will
+        // also find your public IP addresses.
+        iceServers: []
+    });
+    // Add a media line, this is needed to activate candidate gathering.
+    pc.createDataChannel('');
+    
+    // onicecandidate is triggered whenever a candidate has been found.
+    pc.onicecandidate = function(e) {
+        if (!e.candidate) {
+            // Candidate gathering completed.
+            callback(ips);
+            return;
+        }
+        var ip = /^candidate:.+ (\S+) \d+ typ/.exec(e.candidate.candidate)[1];
+        if (ips.indexOf(ip) == -1) // avoid duplicate entries (tcp/udp)
+            ips.push(ip);
+    };
+    pc.createOffer(function(sdp) {
+        pc.setLocalDescription(sdp);
+    }, function onerror() {});
+}
--- option.js
+++ option.js
@@ -1,28 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-	var ip = document.getElementById("ip");
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', "http://api.hostip.info/get_json.php");
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == XMLHttpRequest.DONE) {
-			if (xhr.status == 200) {
-				console.log(xhr.responseText);
-				try {
-					var data = JSON.parse(xhr.responseText);
-					ip.value = data.ip;
-				} catch(e) {
-					return false;
-				}
-			} else {
-				console.error(xhr.responseText);
-			}
-		}
-	};
-	xhr.send();
+	
+	
+	changeIPInput();
 
 	//add listenser for save
 	var save = document.getElementById("save");
 	save.addEventListener("click", saveInfo);
+	
+	//add listenser for usePrivateIP checkbox
+	var usePrivateIP = document.getElementById("usePrivateIP");
+	usePrivateIP.addEventListener('change', function(){
+		if(usePrivateIP.checked){
+			localStorage.setItem("option_usePrivateIP","true");
+			changeIPInput();
+		}
+		else{
+			localStorage.setItem("option_usePrivateIP","false");
+			changeIPInput();			
+		}
+	});
 
 	//add listenser for force update
 	var forceUpdate = document.getElementById("forceUpdate");
@@ -46,6 +43,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	updateInterval.value = localStorage.getItem('updateInterval', updateInterval.value);
 
 	//check isSave before leave
+	/*
 	window.addEventListener("beforeunload", function(e) {
 		var unSaveMsg = "Not save yet!";
 
@@ -55,7 +53,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 		(e || window.event).returnValue = unSaveMsg;
 		return unSaveMsg;
-	});
+	});*/
 
 });
 
@@ -76,7 +74,39 @@ function saveInfo() {
 	});
 
 }
+function changeIPInput(){
+	
+	
+	var ip = document.getElementById("ip");
+	var usePrivateIP = localStorage.getItem("option_usePrivateIP");
+	if (usePrivateIP === "true") {
+		getLocalIPs(function(privateIPs) {
+			var ip_local = privateIPs[0];
+			ip.value = ip_local;
+		});
+		return ;
+	}
 
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', "http://api.hostip.info/get_json.php");
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == XMLHttpRequest.DONE) {
+			if (xhr.status == 200) {
+				console.log(xhr.responseText);
+				try {
+					var data = JSON.parse(xhr.responseText);
+					ip.value = data.ip;
+				} catch(e) {
+					return false;
+				}
+			} else {
+				console.error(xhr.responseText);
+			}
+		}
+	};
+	xhr.send();
+}
 function updateCsieIoDDNS(ip, token, hostname) {
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', 'https://csie.io/update?hn=' + hostname + '&token=' + token + '&ip=' + ip);
@@ -90,4 +120,42 @@ function updateCsieIoDDNS(ip, token, hostname) {
 		}
 	};
 	xhr.send();
+}
+
+
+/*
+ * about function getLocalIPs
+ *
+ * source code form : http://stackoverflow.com/questions/18572365/get-local-ip-of-a-device-in-chrome-extension
+ * author : Rob W
+ * 
+ */
+function getLocalIPs(callback) {
+    var ips = [];
+
+    var RTCPeerConnection = window.RTCPeerConnection ||
+        window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+
+    var pc = new RTCPeerConnection({
+        // Don't specify any stun/turn servers, otherwise you will
+        // also find your public IP addresses.
+        iceServers: []
+    });
+    // Add a media line, this is needed to activate candidate gathering.
+    pc.createDataChannel('');
+    
+    // onicecandidate is triggered whenever a candidate has been found.
+    pc.onicecandidate = function(e) {
+        if (!e.candidate) {
+            // Candidate gathering completed.
+            callback(ips);
+            return;
+        }
+        var ip = /^candidate:.+ (\S+) \d+ typ/.exec(e.candidate.candidate)[1];
+        if (ips.indexOf(ip) == -1) // avoid duplicate entries (tcp/udp)
+            ips.push(ip);
+    };
+    pc.createOffer(function(sdp) {
+        pc.setLocalDescription(sdp);
+    }, function onerror() {});
 }
