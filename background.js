9ba7433052f32fc6dc4319d4ce47680bad033a7e
sintan <david1992309@gmail.com>
2015-08-04 13:20:45 +0800

    simple use UI;add Line notification

--- background.js
+++ background.js
@@ -1,35 +1,143 @@
 var alarmName = "updateDDNS";
-
-chrome.storage.local.get("info",function(obj){
-		var info=obj.info;
-		var updateInterval;
-		updateInterval = info.updateInterval || 60;
-		chrome.alarms.create(alarmName, {
-				delayInMinutes : parseInt(updateInterval),
-				periodInMinutes : parseInt(updateInterval)
+chrome.storage.local.get("powerStatus",function(obj){
+	var powerStatus = obj.powerStatus;
+	if (powerStatus) {
+		chrome.alarms.get("updateDDNS",function(alarm){
+			if(alarm) {
+				init();
+			}
 		});
+	}
 });
-chrome.alarms.onAlarm.addListener(function(alarm) {
-	console.log(alarm,alarm.name);
+chrome.alarms.onAlarm.addListener(function (alarm) {
+	console.log(alarm, alarm.name);
 	if (alarm.name !== "updateDDNS") {
 		return false;
 	}
-	chrome.storage.local.get("info",function(obj){
-			var info=obj.info;
+	chrome.storage.local.get(["info", "powerStatus"], function (obj) {
+		var powerStatus =obj.powerStatus;
+		if(!powerStatus){
+			chrome.alarms.clear("updateDDNS", function (wasCleared) {
+				console.log("updateDDNS alarm clear");
+			});
+			return;
+		}
+		var info = obj.info;
 		var token = info.token;
 		var hostname = info.hostname;
+		var oldIP = info.oldIP || "";
 		if (token && hostname) {
-			updateCsieIoDDNS( token, hostname);
+			//get IP to reduce duplicate update
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', 'http://checkip.amazonaws.com/' );
+			xhr.onreadystatechange = function(){
+				if (xhr.readyState == XMLHttpRequest.DONE) {
+					if(xhr.status == 200) {
+						if(oldIP !== xhr.responseText) {
+							info.oldIP = xhr.responseText;
+							chrome.storage.local.set({'info':info},function(){
+								updateCsieIoDDNS(token, hostname);
+							});
+
+						}
+					}
+					else{//if we can't IP, still update , server not need IP
+						updateCsieIoDDNS(token, hostname);
+					}
+					/* */
+
+				}
+			};
+			xhr.send();
 		} else {
 			console.error("onAlarm:  token or hostname setting error");
-			chrome.alarms.clear("updateDDNS", function(wasCleared) {
+			chrome.alarms.clear("updateDDNS", function (wasCleared) {
 				console.log("updateDDNS alarm clear");
 			});
 		}
 	});
-
 });
+function init() {
+	chrome.storage.local.get("info", function (obj) {
+		var info = obj.info;
+		var updateInterval;
+		updateInterval = info.updateInterval || 60;
+		chrome.alarms.create(alarmName, {
+			delayInMinutes: parseInt(updateInterval),
+			periodInMinutes: parseInt(updateInterval)
+		});
+	});
+}
 
+
+function updateCsieIoDDNS(token, hostname, noti) {//params.noti can only be true or not set
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'https://csie.io/update?hn=' + hostname + '&token=' + token + '&ip=' );
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == XMLHttpRequest.DONE) {
+			if (xhr.status == 200) {
+				chrome.storage.local.get("info",function(obj){
+					var info=obj.info || {};
+					if (info.option_useLineNotification || info.option_useLocalNotification ) {
+						noti = {};
+						noti.useLocal = info.option_useLocalNotification;
+						if(info.lineToken){
+							if(info.lineToken !== "") {
+								noti.useLine = info.option_useLineNotification;
+								noti.lineToken = info.lineToken;
+							}
+						}
+
+						notification(xhr.responseText , noti);
+					}
+				});
+			} else {
+				console.error(xhr.responseText);
+			}
+		}
+	};
+	xhr.send();
+}
+/**
+ *
+ * @param {string} data
+ * @param {{useLocal:Boolean, useLine: Boolean, lineToken: string}} notiOptions
+ */
+function notification(data , notiOptions) {
+	if(notiOptions.useLocal || !notiOptions) {
+		var mesg = "fail";
+		if (data == "OK") {
+			mesg = "csie.io DDNS update success";
+		} else if (data == "KO") {
+			mesg = "Wrong token or hostname!\nPlease check again.";
+		}
+		var opt = {
+			type: "basic",
+			title: "csie.io DDNS update",
+			message: mesg,
+			iconUrl: "img/csieio-128.png"
+
+		};
+
+		chrome.notifications.create("id" + Math.random(), opt, function (id) {
+			console.log(id);
+		});
+	}
+	if(notiOptions.useLine && notiOptions.lineToken != ""){
+		chrome.storage.local.get( "info", function (obj) {
+			var ip = obj.info.oldIP || "";
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', 'https://csie.io/lineme?token=' + notiOptions.lineToken + "&msg=" + "偵測到IP改變，新的IP為:" + ip);
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState == XMLHttpRequest.DONE) {
+				}
+			};
+			xhr.send();
+		});
+	}
+
+}
+/*
 function updateCsieIoDDNS(token, hostname , noti) {//params.noti can only be true or not set, just for force update
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', 'https://csie.io/update?hn=' + hostname + '&token=' + token + '&ip=' );
@@ -39,7 +147,7 @@ function updateCsieIoDDNS(token, hostname , noti) {//params.noti can only be tru
 				//console.log(xhr.responseText);
 				chrome.storage.local.get("info",function(obj){
 						var info=obj.info || {};
-						noti = noti || info.option_useNotification;
+						noti = noti || info.option_useLocalNotification;
 						if(noti == true){
 							notification(xhr.responseText);
 						}
@@ -73,6 +181,7 @@ function notification(data){
 	});
 
 }
+*/
 chrome.app.runtime.onLaunched.addListener(function(){
 		chrome.app.window.create('option.html', {
 				'innerBounds': {
--- option.js
+++ option.js
@@ -1,31 +1,86 @@
 document.addEventListener('DOMContentLoaded', function() {
-		chrome.storage.local.get("info",function(obj){
+		chrome.storage.local.get(["info", "powerStatus"],function(obj){
 				var info=obj.info||{};
+				info.powerStatus = obj.powerStatus;
 				init(info);
 		});
 });
+function ddnsStop(){
+	var powerStatus = document.getElementById("powerStatus");
+	powerStatus.className = "powerOFF";
+	powerStatus.src = chrome.runtime.getURL("img/power_off.png");
+	powerStatus.title = "Not running";
+	chrome.storage.local.set({'powerStatus':false});
+	chrome.alarms.clear("updateDDNS");
+	cancelBackground();
+}
+function ddnsStart(){
+	askBackground(function(){
+		if(chrome.runtime.lastError){
+			ddnsStop();
+		}
+		chrome.storage.local.get("info",function(obj){
+			var info=obj.info||{};
+			var updateInterval = info.updateInterval || 60;
+			if(isEmptyObject(info)){
+				saveInfo();
+			}
+			chrome.alarms.create("updateDDNS", {
+				delayInMinutes : parseInt(updateInterval),
+				periodInMinutes : parseInt(updateInterval)
+			});
+			chrome.alarms.get("updateDDNS",function(alarm){
+				if(alarm){
+					var powerStatus = document.getElementById("powerStatus");
+					powerStatus.className = "powerON";
+					powerStatus.src=powerStatus.src=chrome.runtime.getURL("img/power_on.png");
+					powerStatus.title = "正在運行中...";
+					console.log(alarm);
+				}
+				else{
+					console.log("no alarm:"+alarm);
+				}
+			});
+			updateCsieIoDDNS(info.token,info.hostname, true);
+		});
+		chrome.storage.local.set({'powerStatus':true});
+
+	});
+
+
+
+
+}
 function init(info){
 	//add listenser for save
 	var save = document.getElementById("save");
 
 	save.addEventListener("click", saveInfo);
 
-
+	//load state for isNotification
+	var powerStatus = document.getElementById("powerStatus");
+	if(info.powerStatus == true){
+		ddnsStart();
+	}
+	else{
+		ddnsStop();
+	}
+	powerStatus.addEventListener("click", function() {
+		if(powerStatus.className === "powerON"){
+			ddnsStop();
+		}else{
+			ddnsStart();
+		}
+	});
 	//load state for isNotification
 	var useNotification = document.getElementById("useNotification");
-	useNotification.checked = (info.option_useNotification ) ? true : false;
-	//load state for isNotification
-	var useBackground = document.getElementById("useBackground");
-	useBackground.checked = (info.option_useBackground ) ? true : false;
+	useNotification.checked = (info.option_useLocalNotification ) ? true : false;
 
-	//add listenser for force update
-	var forceUpdate = document.getElementById("forceUpdate");
-	forceUpdate.addEventListener("click", function() {
-		var hostname = document.getElementById("hostname");
-		var token = document.getElementById("token");
-		var updateInterval = document.getElementById("updateInterval");
-		updateCsieIoDDNS(token.value, hostname.value, "true");
-	});
+	//load state for line Notification
+	var useLineNotification = document.getElementById("useLineNotification");
+	useLineNotification.checked = (info.option_useLineNotification ) ? true : false;
+	var lineToken = document.getElementById("lineToken");
+	lineToken.value = info.lineToken || "";
 
 	//display saved value
 	var hostname = document.getElementById("hostname");
@@ -43,51 +98,63 @@ function init(info){
 	advOptionTopbar.addEventListener("click" , function(e){
 		if(advOptContainer.style && advOptContainer.style.display == "none"){
 			advOptContainer.style.display = "block";
-			advOptionTopbar.innerHTML = "- advanced options";
+			advOptionTopbar.innerHTML = '-' + advOptionTopbar.innerText.slice(1);
 		}else{
 			advOptContainer.style.display = "none";
-			advOptionTopbar.innerHTML = "+ advanced options";
+			advOptionTopbar.innerHTML = '+' + advOptionTopbar.innerText.slice(1);
 		}
 		e.stopPropagation();
 	}, false);
 
 }
-function askBackground() {
+function askBackground(callback) {
 
 	// Permissions must be requested from inside a user gesture, like a button's
 	// click handler.
-	chrome.permissions.request({
-		permissions : ['background']
-	}, function(granted) {
-		// The callback argument will be true if the user granted the permissions.
-		if (granted) {
-			var opt = {
-				type : "basic",
-				title : "csie.io DDNS update",
-				message : "run in background",
-				iconUrl : "img/csieio-128.png"
-			};
+	chrome.permissions.contains({
+		permissions: ['background']
+	}, function(result) {
+		if (result) {
+			if(callback) callback();
+		}
+		else {
+			chrome.permissions.request({
+				permissions : ['background']
+			}, function(granted) {
+				// The callback argument will be true if the user granted the permissions.
+				var opt={};
+				if (granted) {
+					opt = {
+						type : "basic",
+						title : "csie.io DDNS update",
+						message : "Power ON",
+						iconUrl : "img/csieio-128.png"
+					};
 
-			chrome.notifications.create("id" + Math.random(), opt, function(id) {
-				console.log(id);
-			});
-		} else {
-			var opt = {
-				type : "basic",
-				title : "csie.io DDNS update",
-				message : "request background permissions fail",
-				iconUrl : "img/csieio-128.png"
+					chrome.notifications.create("id" + Math.random(), opt, function(id) {
+						console.log(id);
+					});
+					if(callback) callback();
+				} else {
+					opt = {
+						type : "basic",
+						title : "csie.io DDNS update",
+						message : "request background permissions fail",
+						iconUrl : "img/csieio-128.png"
 
-			};
+					};
 
-			chrome.notifications.create("id" + Math.random(), opt, function(id) {
-				console.log(id);
+					chrome.notifications.create("id" + Math.random(), opt, function(id) {
+						console.log(id);
+					});
+				}
 			});
 		}
 	});
+
 }
 
-function cancelBackground() {
+function cancelBackground(callback) {
 	chrome.permissions.remove({
 		permissions : ['background'],
 	}, function(removed) {
@@ -95,12 +162,13 @@ function cancelBackground() {
 			var opt = {
 				type : "basic",
 				title : "csie.io DDNS update",
-				message : "Not run in background",
+				message : "Power OFF",
 				iconUrl : "img/csieio-128.png"
 			};
 			chrome.notifications.create("id" + Math.random(), opt, function(id) {
 				console.log(id);
 			});
+			if(callback) callback();
 		} else {
 		}
 	});
@@ -110,32 +178,51 @@ function saveInfo() {
 	var hostname = document.getElementById("hostname");
 	var token = document.getElementById("token");
 	var updateInterval = document.getElementById("updateInterval");
-	var useBackground = document.getElementById("useBackground");
-
-	if (useBackground.checked) {
-		askBackground();
-	} else {
-		cancelBackground();
-	}
-	var info={}
+	var useNotification = document.getElementById("useNotification");
+	var useLineNotification = document.getElementById("useLineNotification");
+	var lineToken = document.getElementById("lineToken");
+	var info={};
+	//TODO save oldIP;
 
 
-	info["option_useNotification"]= useNotification.checked;
-	info["option_useBackground"]= useBackground.checked;
+
+	info["option_useLocalNotification"]= useNotification.checked;
+	info["option_useLineNotification"]= useLineNotification.checked;
+	info["lineToken"] = lineToken.value;
 	info['hostname']= hostname.value;
 	info['token']= token.value;
 	info['updateInterval']= updateInterval.value;
-	chrome.storage.local.set({'info':info});
 
 	chrome.alarms.create("updateDDNS", {
 		delayInMinutes : parseInt(updateInterval.value),
 		periodInMinutes : parseInt(updateInterval.value)
 	});
 
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'http://checkip.amazonaws.com/' );
+	xhr.onreadystatechange = function(){
+		if (xhr.readyState == XMLHttpRequest.DONE) {
+			if(xhr.status == 200) {
+				info.oldIP = xhr.responseText;
+			}
+			else{
+				console.log(xhr.responseText);
+			}
+			chrome.storage.local.set({'info':info},function(){
+				if(chrome.runtime.lastError){
+					notification("save error!");
+					return;
+				}
+			});
+		}
+	};
+	xhr.send();
+
+
 }
 
 
-function updateCsieIoDDNS(token, hostname, noti) {//params.noti can only be true or not set, just for force update
+function updateCsieIoDDNS(token, hostname, noti) {//params.noti can only be true or not set
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', 'https://csie.io/update?hn=' + hostname + '&token=' + token + '&ip=' );
 	xhr.onreadystatechange = function() {
@@ -143,9 +230,10 @@ function updateCsieIoDDNS(token, hostname, noti) {//params.noti can only be true
 			if (xhr.status == 200) {
 					chrome.storage.local.get("info",function(obj){
 							var info=obj.info || {};
-							noti = noti || info.option_useNotification;
 							if (noti ) {
-								notification(xhr.responseText);
+								noti = {};
+								noti.useLocal = info.option_useLocalNotification;
+								notification(xhr.responseText , noti);
 							}
 					});
 			} else {
@@ -155,25 +243,33 @@ function updateCsieIoDDNS(token, hostname, noti) {//params.noti can only be true
 	};
 	xhr.send();
 }
+/**
+ *
+ * @param {string} data
+ * @param {{useLocal:Boolean, useLine: Boolean}} notiOptions
+ */
+function notification(data , notiOptions) {
+	if(notiOptions.useLocal || !notiOptions) {
+		var mesg = "fail";
+		if (data == "OK") {
+			mesg = "csie.io DDNS update success";
+		} else if (data == "KO") {
+			mesg = "Wrong token or hostname!\nPlease check again.";
+		}
+		var opt = {
+			type: "basic",
+			title: "csie.io DDNS update",
+			message: mesg,
+			iconUrl: "img/csieio-128.png"
 
-function notification(data) {
+		};
 
-	var mesg = "fail";
-	if (data == "OK") {
-		mesg = "csie.io DDNS success";
-	} else if (data == "KO") {
-		mesg = "Wrong token or hostname!\nPlease check again.";
+		chrome.notifications.create("id" + Math.random(), opt, function (id) {
+			console.log(id);
+		});
 	}
-	var opt = {
-		type : "basic",
-		title : "csie.io DDNS update",
-		message : mesg,
-		iconUrl : "img/csieio-128.png"
 
-	};
-
-	chrome.notifications.create("id" + Math.random(), opt, function(id) {
-		console.log(id);
-	});
-
+}
+function isEmptyObject(obj){
+	return Object.keys(obj).length===0
 }
